ab7f66da58b95f35d1b92f33b6425663703bc0e4
Alex Franklin <roaringdragon02@gmail.com>
2020-05-30 12:20:22 -0700

    Add build to repo

---
+++ build/precache-manifest.2993391ea1248261b3232cf799f0a0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92999e69b1654ba5069493abd40e8ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b27bcdeff26f5616202",
+    "url": "/static/css/main.0867f900.chunk.css"
+  },
+  {
+    "revision": "00ef0a62b61d6a2f5af1",
+    "url": "/static/js/2.ec1bd959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec1bd959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b27bcdeff26f5616202",
+    "url": "/static/js/main.8ef0712e.chunk.js"
+  },
+  {
+    "revision": "899bdbedf54aae190a09",
+    "url": "/static/js/runtime-main.394f33c0.js"
+  }
+]);
