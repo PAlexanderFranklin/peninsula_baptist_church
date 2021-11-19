a291429c75ffe5de496dab9777d38bee8d7a97c3
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-11-19 02:49:36 -0800

    Add some Skynet setup

--- src/views/App.js
+++ src/views/App.js
@@ -9,23 +9,24 @@ import Gallery from './Pages/Gallery/Gallery'
 import VaccineStatement from './Pages/VaccineStatement/VaccineStatement'
 import Admin from './Admin/Admin'
 import {Route} from 'react-router-dom'
-
-// metadatadb: https://siasky.net/DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA
+import { SkynetProvider } from '../state/SkynetContext';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Route path="/" exact component={Home} />
-        <Route path="/sermons" component={Sermons} />
-        <Route path="/visit" component={Visit} />
-        <Route path="/events" component={Events} />
-        <Route path="/gallery" component={Gallery} />
-        <Route path="/statement-on-vaccines" component={VaccineStatement} />
-        <Route path="/admin" component={Admin} />
+    <SkynetProvider>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Route path="/" exact component={Home} />
+          <Route path="/sermons" component={Sermons} />
+          <Route path="/visit" component={Visit} />
+          <Route path="/events" component={Events} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/statement-on-vaccines" component={VaccineStatement} />
+          <Route path="/admin" component={Admin} />
+        </div>
       </div>
-    </div>
+    </SkynetProvider>
   );
 }
 
--- src/views/Pages/Sermons/Sermons.js
+++ src/views/Pages/Sermons/Sermons.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// useContext,
 import './Sermons.css';
 import Row from "./Row";
+// import { SkynetContext } from '../../../state/SkynetContext';
 
-function Sermons(props) {
+// metadatadb: https://siasky.net/DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA
 
-  let sermonData = [];
-  for(let i = 0; i < 50; i++) {
-    sermonData.push({
-      file_name: "EM100521",
-      title: "test title",
-      series: "this is a series title",
-      url: "https://siasky.net/_A3BzfJhXm2aaPkE9mNQecOPxShKDUdoVHqurZ2ze-kjoA",
-      book: "Galatians",
-      verses: "5:13-18",
-      speaker: "Ethan Hardy",
-      date: (211005 + i) + ''
-    })
-  }
+function Sermons() {
+
+  // const client = useContext(SkynetContext);
+  // const [ dataBase, setDataBase ] = useState();
+  const [ sermonData, setSermonData ] = useState([]);
+
+  // useEffect(() => {
+  //   async function getDatabase() {
+  //     try {
+  //       const response = await client.getFileContent("DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA");
+  //       console.log(response)
+  //       setDataBase(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   getDatabase();
+  // }, [client]);
+
+  // Example Data
+  useEffect(() => {
+    for (let i = 0; i < 50; i++) {
+      let sampleSermonData = [];
+      sampleSermonData.push({
+        file_name: "EM100521",
+        title: "test title",
+        series: "this is a series title",
+        url: "https://siasky.net/_A3BzfJhXm2aaPkE9mNQecOPxShKDUdoVHqurZ2ze-kjoA",
+        book: "Galatians",
+        verses: "5:13-18",
+        speaker: "Ethan Hardy",
+        date: (211005 + i) + ''
+      })
+      setSermonData(sampleSermonData);
+    }
+  }, [setSermonData]);
 
   return (
     <div className="Sermons bubble">
