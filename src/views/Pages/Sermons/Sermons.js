ddeca27da0e9d34f847de2a3b62222ded8129e9c
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2021-11-26 15:24:36 -0800

    Resort to fetching the database, Add SQL query

--- src/views/Pages/Sermons/Sermons.js
+++ src/views/Pages/Sermons/Sermons.js
@@ -6,52 +6,82 @@ import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/
 
 import './Sermons.css';
 import Row from "./Row";
-import { SkynetContext } from '../../../state/SkynetContext';
+// import { SkynetContext } from '../../../state/SkynetContext';
 
 // metadatadb: https://siasky.net/DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA
 
 function Sermons() {
 
-  const client = useContext(SkynetContext);
-  const [dataBaseFile, setDataBaseFile] = useState(null);
-  const [db, setDb] = useState(null);
+  // const client = useContext(SkynetContext);
+  const [ dataBaseFile, setDataBaseFile ] = useState(null);
+  const [ db, setDb ] = useState(null);
   const [ sermonData, setSermonData ] = useState([]);
+
+  // useEffect(() => {
+  //   async function getDatabase() {
+  //     try {
+  //       const response = await client.getFileContent("DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA");
+  //       console.log(response.data);
+  //       setDataBaseFile(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   getDatabase();
+  // }, [client]);
 
   useEffect(() => {
     async function getDatabase() {
       try {
-        const response = await client.getFileContent("DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA");
-        setDataBaseFile(response.data);
-        console.log(response);
+        const SQL = await initSqlJs({ locateFile: () => sqlWasm });
+        const dataPromise = fetch("https://siasky.net/DABchy1Q3tBUggIP9IF_7ha9vAfBZ1d2aYRxUnHSQg9QNA").then(res => res.arrayBuffer());
+        const buf = await Promise.resolve(dataPromise)
+        setDb(new SQL.Database(new Uint8Array(buf)));
       } catch (error) {
         console.log(error);
       }
     }
     getDatabase();
-  }, [client]);
+  }, []);
 
-  useEffect(() => {
-      async function initDB() {
-        // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
-        // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
-        // see ../craco.config.js
-        try {
-          if (dataBaseFile) {
-            const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-            setDb(new SQL.Database(dataBaseFile));
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      initDB();
-  }, [dataBaseFile]);
+  // useEffect(() => {
+  //     async function initDB() {
+  //       // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
+  //       // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
+  //       // see ../craco.config.js
+  //       try {
+  //         if (dataBaseFile) {
+  //           const SQL = await initSqlJs({ locateFile: () => sqlWasm });
+  //           setDb(new SQL.Database(dataBaseFile));
+  //         }
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     }
+  //     initDB();
+  // }, [dataBaseFile]);
 
   useEffect(() => {
       async function runQuery() {
         try {
           if (db) {
-            const response = db.exec("SELECT * FROM audio");
+            const response = db.exec(`
+              SELECT
+                file_name,
+                title,
+                books.name AS book,
+                verse,
+                series.name AS series,
+                speakers.name AS speaker,
+                date,
+                skylink,
+                notes
+              FROM
+                audio
+                LEFT JOIN books ON books.id = audio.book_id
+                LEFT JOIN series ON series.id = audio.series_id
+                LEFT JOIN speakers ON speakers.id = audio.speaker_id
+            `);
             console.log(response);
           }
         } catch (err) {
@@ -66,7 +96,7 @@ function Sermons() {
     let sampleSermonData = [];
     for (let i = 0; i < 50; i++) {
       sampleSermonData.push({
-        file_name: "EM100521",
+        file_name: "EM1005" + i,
         title: "test title",
         series: "this is a series title",
         url: "https://siasky.net/_A3BzfJhXm2aaPkE9mNQecOPxShKDUdoVHqurZ2ze-kjoA",
