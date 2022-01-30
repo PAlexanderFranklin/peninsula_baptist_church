16291039f8878a95186edf2bd3021821b0dab5de
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-01-29 20:43:41 -0800

    Add Page Count

---
+++ src/views/Pages/Sermons/PageNumbers.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './PageNumbers.css';
+
+function PageNumbers(props) {
+
+  const { rowCount, queryOptions, setQueryOptions } = props;
+
+  let pageCount = Math.ceil(rowCount / 10);
+  let pageButtons = [];
+
+  for (let i = 1; i <= pageCount; i++) {
+    let classNames = "page_button"
+    if (i === queryOptions.page) {
+      classNames = classNames + " active_page_button"
+    }
+    pageButtons.push(
+    <button
+      className={classNames}
+      onClick={() => {setQueryOptions({...queryOptions, page: i})}}
+    >
+      {i}
+    </button>)
+  }
+
+  return (
+    <div className="PageNumbers">
+      {pageButtons}
+    </div>
+  );
+}
+
+export default PageNumbers;
--- src/views/Pages/Sermons/Sermons.js
+++ src/views/Pages/Sermons/Sermons.js
@@ -5,6 +5,7 @@ import initSqlJs from 'sql.js';
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
 
 import './Sermons.css';
+import PageNumbers from "./PageNumbers";
 import Row from "./Row";
 import { SkynetContext } from '../../../state/SkynetContext';
 
@@ -15,13 +16,20 @@ function Sermons() {
   const [ db, setDb ] = useState(null);
   const [ sermonData, setSermonData ] = useState(null);
   const [ errorState, setErrorState ] = useState(null);
+  const [ rowCount, setRowCount ] = useState(0);
   const [ queryFilter, setQueryFilter ] = useState(
     {
       title: ``,
       book: ``,
       series: ``,
       speaker: ``,
+    }
+  );
+  const [ queryOptions, setQueryOptions ] = useState(
+    {
       sort: `date DESC`,
+      page: `1`,
+      rowsPerPage: `10`,
     }
   );
   
@@ -87,7 +95,8 @@ function Sermons() {
               ${queryFilter.book}
               ${queryFilter.series}
               ${queryFilter.speaker}
-            ORDER BY ${queryFilter.sort};
+            ORDER BY ${queryOptions.sort}
+            LIMIT ${queryOptions.rowsPerPage} OFFSET ${(queryOptions.page - 1) * queryOptions.rowsPerPage};
           `);
           const columns = response[0].columns;
           const values = response[0].values;
@@ -107,54 +116,48 @@ function Sermons() {
       }
     }
     runQuery();
-  }, [db, queryFilter]);
+  }, [db, queryFilter, queryOptions]);
 
-  // useEffect(() => {
-  //   async function getRowCount() {
-  //     try {
-  //       if (db) {
-  //         const response = db.exec(`
-  //           SELECT
-  //             COUNT(skylink) AS row_count
-  //           FROM
-  //             audio
-  //             LEFT JOIN books ON books.id = audio.book_id
-  //             LEFT JOIN series ON series.id = audio.series_id
-  //             LEFT JOIN speakers ON speakers.id = audio.speaker_id
-  //           WHERE
-  //             skylink IS NOT NULL
-  //             ${queryFilter.title}
-  //             ${queryFilter.book}
-  //             ${queryFilter.series}
-  //             ${queryFilter.speaker}
-  //           ORDER BY ${queryFilter.sort};
-  //         `);
-  //         const columns = response[0].columns;
-  //         const values = response[0].values;
-  //         let rows = [];
-  //         values.forEach(element => {
-  //           let row = {};
-  //           for (let i = 0; i < columns.length; i++) {
-  //             row[columns[i]] = element[i];
-  //           }
-  //           rows.push(row);
-  //         });
-  //         setSermonData(rows);
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //       setErrorState("Failed to load sermons.");
-  //     }
-  //   }
-  //   getRowCount();
-  // }, [db, queryFilter]);
+  useEffect(() => {
+    async function getRowCount() {
+      try {
+        if (db) {
+          const response = db.exec(`
+            SELECT
+              COUNT(skylink) AS row_count
+            FROM
+              audio
+              LEFT JOIN books ON books.id = audio.book_id
+              LEFT JOIN series ON series.id = audio.series_id
+              LEFT JOIN speakers ON speakers.id = audio.speaker_id
+            WHERE
+              skylink IS NOT NULL
+              ${queryFilter.title}
+              ${queryFilter.book}
+              ${queryFilter.series}
+              ${queryFilter.speaker};
+          `);
+          setRowCount(response[0].values[0][0]);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getRowCount();
+  }, [db, queryFilter]);
 
   return (
     <div className="Sermons bubble">
       { sermonData ?
         [
           <div className='sermons_options'>
+            {rowCount}
           </div>,
+          <PageNumbers
+            rowCount={rowCount}
+            queryOptions={queryOptions}
+            setQueryOptions={setQueryOptions}
+          />,
           sermonData.map(element => 
             <Row
               key={element.file_name}
@@ -164,7 +167,12 @@ function Sermons() {
               passage={element.book + " " + element.verse}
               speaker={element.speaker}
               date={element.date}/>
-          )
+          ),
+          <PageNumbers
+            rowCount={rowCount}
+            queryOptions={queryOptions}
+            setQueryOptions={setQueryOptions}
+          />
         ]
         : <div>{errorState ?
           <div className="error_container">
