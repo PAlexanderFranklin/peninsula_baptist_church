5484c29aba278ca73491d9a6bc6d567cb2b6274b
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-03-03 00:21:59 -0800

    Switch to a smaller portal

--- src/state/SkynetContext.js
+++ src/state/SkynetContext.js
@@ -5,7 +5,7 @@ const SkynetContext = createContext(undefined);
 
 // We'll define a portal to allow for developing on localhost.
 // When hosted on a skynet portal, SkynetClient doesn't need any arguments.
-const portal = 'https://siasky.net';
+const portal = 'https://fileportal.org';
 
 // Initiate the SkynetClient
 const client = new SkynetClient(portal);
